6e5506c08c0a1707ff9e64cf205fe9193cbad296
Toluddev <toluwalaseoludipe7@gmail.com>
2025-06-04 23:03:46 +0100

    fixes

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -30,10 +30,6 @@ const IntroSection: React.FC = () => {
         
         <Link to="/about" className="inline-block border-b border-black text-black uppercase tracking-widest text-sm hover:text-gray-600 transition-colors">
           LEARN MORE
-
-
-
-          
         </Link>
       </div>
     </section>
