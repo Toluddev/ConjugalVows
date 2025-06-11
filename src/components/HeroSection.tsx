c10dcf45c32bdb5a6930d9e31c67b9fdc159f744
Toluddev <toluwalaseoludipe7@gmail.com>
2025-06-11 23:05:38 +0100

    m

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,8 +19,6 @@ const HeroSection: React.FC<HeroSectionProps> = ({ videoSrc, logoLarge = true })
       >
         <source src={bgvideo} type="video/mp4" />
         Your browser does not support the video tag.
-
-        
       </video>
       
       {/* Overlay */}
